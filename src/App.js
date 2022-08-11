42f334149f5486cb5fa2daab02a483b5adbc7f69
Alireza <alirezwa03@gmail.com>
2022-08-11 11:41:08 +0430

    remove default code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <>
-    </>
+    <div>
+      
+    </div>
   );
-}
+};
 
 export default App;
