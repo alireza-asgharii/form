1d9573e496d0a5f357c35ca01d543ec58c2064df
Alireza <alirezwa03@gmail.com>
2022-08-11 15:34:07 +0430

    modified files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Main from './components/Main';
 
 const App = () => {
   return (
     <div>
-      
+      <Main />
     </div>
   );
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Landing from './Landing';
+import styled from 'styled-components';
+
+//Styled-components
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #EAF6F6;
+`
 
 const Main = () => {
   return (
-    <div>
-      
-    </div>
+    <Div>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/*' element={<Navigate />} />
+      </Routes>
+    </Div>
   );
 };
 
