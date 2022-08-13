4269f5cbb098bdb53328de324c813a536a707c6c
Alireza <alirezwa03@gmail.com>
2022-08-13 14:20:28 +0430

    Creat Register Component

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React from "react";
+
+//styles
+import styles from "../styles/register.module.scss";
+import { Link } from "react-router-dom";
+
+//Icons
+// import google from "../assets/icon/google.png";
+// import apple from "../assets/icon/apple.png";
+// import facebook from "../assets/icon/facebook.png";
 
 const Register = () => {
   return (
-    <div>
-      Register
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Hello</h1>
+        <p>Wellcome to your Website </p>
+      </div>
+      <form className={styles.formContainer}>
+        <label>
+          <input type="text" name="name" placeholder="Enter your name" />
+          <span>Err message!</span>
+        </label>
+        <label>
+          <input type="email" name="email" placeholder="Email" />
+          {/* <span>Err message!</span> */}
+        </label>
+        <label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          {/* <span>Err message!</span> */}
+        </label>
+        <label>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm your password"
+          />
+          <span>password is not definde!</span>
+        </label>
+        <label className={styles.checkbox}>
+          <p>Accept the rules</p>
+          <input type="checkbox" name="checkbox" />
+          <span>Err message!</span>
+        </label>
+          <button className={styles.SignUpButton}>Sign up</button>
+      </form>
+      {/* <div className={styles.iconContainer}>
+        <span>Or continue with</span>
+        <span>
+          <img src={google} alt="logo" />
+        </span>
+        <span>
+          <img src={apple} alt="logo" />
+        </span>
+        <span>
+          <img src={facebook} alt="logo" />
+        </span>
+      </div> */}
+      <span className={styles.login}>
+        You have an account? <Link to="/login">Login now</Link>
+      </span>
     </div>
   );
 };
