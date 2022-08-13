c29cc9e1a2350ea9c7d20a01d200b9c897d7e818
Alireza <alirezwa03@gmail.com>
2022-08-13 15:15:11 +0430

    response style for Sign up component

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,51 +4,49 @@ import React from "react";
 import styles from "../styles/register.module.scss";
 import { Link } from "react-router-dom";
 
-//Icons
-// import google from "../assets/icon/google.png";
-// import apple from "../assets/icon/apple.png";
-// import facebook from "../assets/icon/facebook.png";
+
+
 
 const Register = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <h1>Hello</h1>
-        <p>Wellcome to your Website </p>
+      <div className={styles.imageContainer}>
+        {/* image */}
       </div>
-      <form className={styles.formContainer}>
-        <label>
-          <input type="text" name="name" placeholder="Enter your name" />
-          <span>Err message!</span>
-        </label>
-        <label>
-          <input type="email" name="email" placeholder="Email" />
-          {/* <span>Err message!</span> */}
-        </label>
-        <label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-          {/* <span>Err message!</span> */}
-        </label>
-        <label>
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm your password"
-          />
-          <span>password is not definde!</span>
-        </label>
-        <label className={styles.checkbox}>
-          <p>Accept the rules</p>
-          <input type="checkbox" name="checkbox" />
-          <span>Err message!</span>
-        </label>
+      <div className={styles.Signup}>
+        <div className={styles.header}>
+          <h1>Hello</h1>
+          <p>Wellcome to your Website </p>
+        </div>
+        <form className={styles.formContainer}>
+          <label>
+            <input type="text" name="name" placeholder="Enter your name" />
+            <span>Err message!</span>
+          </label>
+          <label>
+            <input type="email" name="email" placeholder="Email" />
+            {/* <span>Err message!</span> */}
+          </label>
+          <label>
+            <input type="password" name="password" placeholder="Password" />
+            {/* <span>Err message!</span> */}
+          </label>
+          <label>
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm your password"
+            />
+            <span>password is not definde!</span>
+          </label>
+          <label className={styles.checkbox}>
+            <p>Accept the rules</p>
+            <input type="checkbox" name="checkbox" />
+            <span>Err message!</span>
+          </label>
           <button className={styles.SignUpButton}>Sign up</button>
-      </form>
-      {/* <div className={styles.iconContainer}>
+        </form>
+        {/* <div className={styles.iconContainer}>
         <span>Or continue with</span>
         <span>
           <img src={google} alt="logo" />
@@ -60,9 +58,10 @@ const Register = () => {
           <img src={facebook} alt="logo" />
         </span>
       </div> */}
-      <span className={styles.login}>
-        You have an account? <Link to="/login">Login now</Link>
-      </span>
+        <span className={styles.login}>
+          You have an account? <Link to="/login">Login now</Link>
+        </span>
+      </div>
     </div>
   );
 };
